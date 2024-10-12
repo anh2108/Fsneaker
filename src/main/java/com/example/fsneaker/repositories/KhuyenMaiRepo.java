911f58f608314f8e16cf697f639828df55cdd1e9
donangluan <donangluan@gmail.com>
2024-10-12 19:01:15 +0700

    Update KhuyenMaiRepo.java
    khuyen mai repositoy

--- src/main/java/com/example/fsneaker/repositories/KhuyenMaiRepo.java
+++ src/main/java/com/example/fsneaker/repositories/KhuyenMaiRepo.java
@@ -1,4 +1,15 @@
 package com.example.fsneaker.repositories;
 
-public interface KhuyenMaiRepo {
-}
+import com.example.fsneaker.entity.KhuyenMai;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface KhuyenMaiRepo extends JpaRepository<KhuyenMai,Integer> {
+    // tìm kiếm theo mã khuyến mại hoặc tên khuyến mại
+    List<KhuyenMai> findByMaKhuyenMaiContainingIgnoreCaseOrTenKhuyenMaiContainingIgnoreCase(String maKhuyenMaiKeyword, String tenKhuyenMaiKeyword);
+
+    KhuyenMai findByMaKhuyenMai(String maKhuyenMai);
+ }
