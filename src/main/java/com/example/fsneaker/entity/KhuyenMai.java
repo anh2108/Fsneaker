f42ced3cda794bfd2330e48935d1e78b40f18e5f
donangluan <donangluan@gmail.com>
2024-10-18 19:38:59 +0700

    Update KhuyenMai.java

--- src/main/java/com/example/fsneaker/entity/KhuyenMai.java
+++ src/main/java/com/example/fsneaker/entity/KhuyenMai.java
@@ -2,6 +2,8 @@ package com.example.fsneaker.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,19 +24,24 @@ public class KhuyenMai {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Integer id;
-    @NotBlank
-    @Column(name="MaKhuyenMai")
+
+    @NotBlank(message = "Mã khuyến mãi không được để trống")
+    @Size(min = 5, max = 10, message = "Mã khuyến mãi phải có từ 5 đến 10 ký tự")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Mã khuyến mãi chỉ chứa chữ và số")
+
+    @Column(name="MaKhuyenMai", unique = true)
     private String maKhuyenMai;
 
-    @NotBlank
+    @NotBlank(message = "Tên khuyến mãi không được để trống")
+    @Size(min = 5, max = 20, message = "Tên khuyến mãi phải có từ 5 đến 20 ký tự")
     @Column(name="TenKhuyenMai")
     private String tenKhuyenMai;
 
-    @NotBlank
+    @NotBlank(message = "Loại khuyến mại khuyến mãi không được để trống")
     @Column(name="LoaiKhuyenMai")
     private String loaiKhuyenMai;
 
-    @NotBlank
+    @NotBlank(message = "Mô tả khuyến mãi không được để trống")
     @Column(name="MoTa")
     private String moTa;
 
